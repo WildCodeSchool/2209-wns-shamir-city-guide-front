dceeb698f0e8f9a226392a0e5ce4c298cf954b85
ThibaultBardinetLanglois <langlois.bardinet.thibault@gmail.com>
2022-11-17 23:54:16 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
+import ITag from "./types/ITag";
+
+const GET_ALL_TAGS = gql`
+  query {
+    getAllTags {
+      name
+    }
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [tags, setTags] = useState<ITag[]>([]);
+
+  const { loading, error } = useQuery(GET_ALL_TAGS, {
+    onCompleted: (data) => {
+      setTags(data.getAllTags);
+    },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  console.log("All tags =>", tags);
+
+  return <div className="App"></div>;
 }
 
 export default App;
