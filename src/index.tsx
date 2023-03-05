ceafd51122b1b5ba9864b7f12c00371b4c700d56
christelle marquier <christellemonv@gmail.com>
2023-03-05 19:14:36 +0100

    mise à jour des branches dev et city

--- src/index.tsx
+++ src/index.tsx
@@ -35,9 +35,7 @@ const authLink = setContext((_, { headers }) => {
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache({
-    addTypename: false
-  }),
+  cache: new InMemoryCache(),
 });
 
 const root = ReactDOM.createRoot(
